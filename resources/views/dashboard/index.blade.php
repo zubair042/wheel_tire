35c20a2a383b6f015bd8f5b9276ae5fac1860480
zubair042 <042zubair@github.com>
2019-05-08 14:09:40 +0500

    commit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,11 @@
-index page
+@extends('layouts.app')
+@section('title', 'asdfasd adsfs')
+
+@section('sidebar')
+    @parent
+
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
