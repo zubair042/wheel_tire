0d280a72b39c00c32928c2e93cdd1d61ed7cc38e
Faisal Saleem <faisalsaleem000786@gmail.com>
2019-05-09 10:29:51 +0500

    Update view_reports.blade.php
    commit

--- resources/views/reports/view_reports.blade.php
+++ resources/views/reports/view_reports.blade.php
@@ -1,5 +1,146 @@
 @extends('layouts.app')
 @section('title', 'Wheel Tire')
 @section('content')
-    <h1>View list reports</h1>
+
+
+<div class="content">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					<div class="col-md-12">
+						<span class="font-weight-semibold" style="font-size: 30px;">Wheel/Tire Installation Report</span>
+						<span class="font-weight-semibold" style="float: right;color: #5543e8;font-size: 15px;">ID: 352602</span>
+					</div>
+				</div>
+				<div class="card-body">
+					<div class="row">
+						<div class="col-md-8">
+							<a href="#" class="d-inline-block">
+								<img src="{{asset('')}}assets/images/placeholders/cover.jpg" class="img-fluid" alt="">
+							</a>
+							<div class="card" style="margin-top:12px;">
+								<div class="card-header">
+									<h6 class="card-title text-secondary">Comments:</h6>
+								</div>
+								<div class="card-body">
+									<apan class="text-primary font-weight-semibold">John Smith : </apan><span>Torque wrench was missing</span>
+									<br>
+									<span class="text-success font-weight-semibold">Bob Jones : </span><span>John say the torque wrench was not in the shop</span> 
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4" style="margin-top: 5px;">
+							<label class="font-weight-black">Work Performed By:</label>
+							<p>Jhon Smith</p>
+							<label class="font-weight-black">Unit Number:</label>
+							<p>422254</p>
+							<label class="font-weight-black">Location:</label>
+							<p>Johar Town</p>
+							<label class="font-weight-black">Wheel Lug Nuts Torqued To:</label>
+							<p>700 lbs</p>
+							<label class="font-weight-black">Second Signature:</label>
+							<p>Bob Jones</p>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+							<button type="button" class="btn btn-primary rounded-round legitRipple">Apply Signature</button>
+							<button type="button" class="btn btn-danger rounded-round legitRipple">Add Comment</button>
+						</div>
+					</div>
+					<div class="card" style="margin-top: 8px;background: #63af81">
+						<div class="card-body">
+							<h4 class="font-weight-semibold" style="text-align: center;color: #f9f9f9;"> LEFT FRONT WHEEL POSITION</h4>
+							<div class="row">
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img src="{{asset('')}}assets/images/placeholders/placeholder.jpg" width="80%" height="100%" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img class="card-img img-fluid" src="{{asset('')}} assets/images/placeholders/placeholder.jpg" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img class="card-img img-fluid" src="{{asset('')}}assets/images/placeholders/placeholder.jpg" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img class="card-img img-fluid" src="{{asset('')}}assets/images/placeholders/placeholder.jpg" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+							</div>
+							<h4 class="font-weight-semibold" style="text-align: center;color: #f9f9f9;margin-top: 12px;"> RIGHT REAR WHEEL POSITION</h4>
+							<div class="row">
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img class="card-img img-fluid" src="{{asset('')}}assets/images/placeholders/placeholder.jpg" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img class="card-img img-fluid" src="{{asset('')}}assets/images/placeholders/placeholder.jpg" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img class="card-img img-fluid" src="{{asset('')}}assets/images/placeholders/placeholder.jpg" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+								<div class="col-md-3">
+									<div class="card-img-actions m-1">
+										<img class="card-img img-fluid" src="{{asset('')}}assets/images/placeholders/placeholder.jpg" alt="">
+										<div class="card-img-actions-overlay card-img">
+											<a href="{{asset('')}}assets/images/placeholders/placeholder.jpg" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" data-popup="lightbox" rel="group">
+												<i class="icon-plus3"></i>
+											</a>
+										</div>
+									</div>	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 @endsection 
