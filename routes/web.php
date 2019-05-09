055b4da5c4569969aab23f3a7002d97c319074d2
zubair042 <042zubair@github.com>
2019-05-09 12:56:04 +0500

    commit

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,9 @@ Route::get('/view_report', function(){
 Route::get('/add_customer', function(){
 	return view('customer/add_customer');
 });
+Route::get('/users', function(){
+	return view('users/index');
+});
+Route::get('/add_user', function(){
+	return view('users/add_user');
+});
