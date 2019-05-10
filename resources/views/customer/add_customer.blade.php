fbb6f986de09c68b2887dcbc94bd49f9ecaea144
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-05-10 11:27:00 +0500

    commit

--- resources/views/customer/add_customer.blade.php
+++ resources/views/customer/add_customer.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-<script src="{{asset('global_assets/js/plugins/forms/styling/switch.min.js')}}"></script>
-<script src="{{asset('global_assets/js/plugins/forms/styling/switchery.min.js')}}"></script>
-<script src="{{ asset('global_assets/js/plugins/forms/styling/uniform.min.js') }}"></script>
-<script src="{{asset('global_assets/js/plugins/forms/selects/select2.min.js')}}"></script>
+<script src="{{asset('js/plugins/forms/styling/switch.min.js')}}"></script>
+<script src="{{asset('js/plugins/forms/styling/switchery.min.js')}}"></script>
+<script src="{{ asset('js/plugins/forms/styling/uniform.min.js') }}"></script>
+<script src="{{asset('js/plugins/forms/selects/select2.min.js')}}"></script>
 
 
 
@@ -23,6 +23,15 @@
 					</div>
 				</div>
 				<div class="row">
+					<div class="col-md-2 offset-md-3">
+						<div class="form-check form-check-switchery form-check-inline form-check-right">
+							<label class="form-check-label">
+								<input type="checkbox" class="form-check-input-switchery" checked="" data-fouc="" data-switchery="true" style="display: none;"><span class="switchery switchery-default" style="background-color: rgb(100, 189, 99); border-color: rgb(100, 189, 99); box-shadow: rgb(100, 189, 99) 0px 0px 0px 10px inset; transition: border 0.4s ease 0s, box-shadow 0.4s ease 0s, background-color 1.2s ease 0s;"><small style="left: 18px; background-color: rgb(255, 255, 255); transition: background-color 0.4s ease 0s, left 0.2s ease 0s;"></small></span>
+								<p style="font-size: 18px;">First input</p>
+							</label>
+						</div>
+					</div>
+				</div>
 			    <div class="row"">
 			    	<div class="col-md-2 offset-md-3">
 						<span class="input-group-text" style="font-size: 18px;">Account Type</span>
