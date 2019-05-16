0209967571b99db63fa60264802ee99682301d02
zubair042 <042zubair@github.com>
2019-05-16 15:08:51 +0500

    commit

--- app/Http/Controllers/Accounts.php
+++ app/Http/Controllers/Accounts.php
@@ -14,6 +14,11 @@ class Accounts extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         $user_id = auth()->user()->id;
