fedb3a8c5cca09506af8d46d3f060172cb4a4f1f
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-05-17 16:47:22 +0500

    commit

--- resources/views/users/edit_user.blade.php
+++ resources/views/users/edit_user.blade.php
@@ -23,80 +23,82 @@
 						<hr align="left" >
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-2 offset-md-3">
-						<span class="input-group-text" style="font-size: 18px;">Activation</span>
-					</div>
-					<div class="col-md-3">
-						<div class="form-check form-check-switch form-check-switch-left">
-							<label class="form-check-label d-flex align-items-center">
-								<input type="checkbox" data-on-text="On" data-off-text="Off" class="form-check-input-switch" data-size="small" checked>
-							</label>
+				<form method="POST" action="{{url('')}}" id="adit_user_form">
+					<div class="row">
+						<div class="col-md-2 offset-md-3">
+							<span class="input-group-text" style="font-size: 18px;">Activation</span>
+						</div>
+						<div class="col-md-3">
+							<div class="form-check form-check-switch form-check-switch-left">
+								<label class="form-check-label d-flex align-items-center">
+									<input type="checkbox" data-on-text="On" data-off-text="Off" class="form-check-input-switch" data-size="small" checked>
+								</label>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="row">
-			    	<div class="col-md-2 offset-md-3">
-						<span class="input-group-text" style="font-size: 18px;">Company Name</span>
-					</div>
-					<div class="col-md-3">
-						<select class="custom-select" name="">
-			                <option value="Manager"><span>Manager</span></option>
-			                <option value="Worker">Worker</option>
-			                <option value="Salesman">Salesman</option>
-			            </select>
-			    	</div>
-			    </div>
-			    <div class="row">
-			    	<div class="col-md-2 offset-md-3">
-						<span class="input-group-text" style="font-size: 18px;">User Roles</span>
-					</div>
-					<div class="col-md-3">
-						<select class="custom-select1" name="">
-			                <option value="Manager"><span>Manager</span></option>
-			                <option value="Worker">Worker</option>
-			                <option value="Salesman">Salesman</option>
-			            </select>
-			    	</div>
-			    </div>
-			    <div class="row">
-			    	<div class="col-md-2 offset-md-3">
-						<span class="input-group-text" style="font-size: 18px;">First Name</span>
-					</div>
-					<div class="col-md-3">
-						<input type="text" name="lbs_weight" id="" class="form-control">
-			    	</div>
-			    </div>
-			    <div class="row">
-			    	<div class="col-md-2 offset-md-3">
-						<span class="input-group-text" style="font-size: 18px;">Last Name</span>
-					</div>
-					<div class="col-md-3">
-						<input type="text" name="lbs_weight" id="" class="form-control">
-			    	</div>
-			    </div>
-			    <div class="row">
-			    	<div class="col-md-2 offset-md-3">
-						<span class="input-group-text" style="font-size: 18px;">Email</span>
-					</div>
-					<div class="col-md-3">
-						<input type="text" name="lbs_weight" id="" class="form-control">
-			    	</div>
-			    </div>
-			    <div class="row">
-			    	<div class="col-md-2 offset-md-3">
-						<span class="input-group-text" style="font-size: 18px;">Password</span>
-					</div>
-					<div class="col-md-3">
-						<input type="text" name="lbs_weight" id="" class="form-control">
-			    	</div>
-			    </div>
-			    <div class="row" style="text-align: center;margin:30px 0;">
-			    	<div class="col-md-9 offset-md-1">
-			    		<button type="button" style="background-color: #4f99c6!important;margin-right: 12px;" class="btn btn-primary legitRipple"><i class="icon-checkmark mr-2"></i>Update</button>
-			    		<button type="button" style="background-color: #8b9aa3!important;margin-left: 12px;" class="btn btn-primary legitRipple"><i class="icon-reset mr-2"></i>Reset</button>
-			    	</div>
-			    </div>
+					<div class="row">
+				    	<div class="col-md-2 offset-md-3">
+							<span class="input-group-text" style="font-size: 18px;">Company Name</span>
+						</div>
+						<div class="col-md-3">
+							<select class="custom-select" name="">
+				                <option value="Manager"><span>Manager</span></option>
+				                <option value="Worker">Worker</option>
+				                <option value="Salesman">Salesman</option>
+				            </select>
+				    	</div>
+				    </div>
+				    <div class="row">
+				    	<div class="col-md-2 offset-md-3">
+							<span class="input-group-text" style="font-size: 18px;">User Roles</span>
+						</div>
+						<div class="col-md-3">
+							<select class="custom-select1" name="">
+				                <option value="Manager"><span>Manager</span></option>
+				                <option value="Worker">Worker</option>
+				                <option value="Salesman">Salesman</option>
+				            </select>
+				    	</div>
+				    </div>
+				    <div class="row">
+				    	<div class="col-md-2 offset-md-3">
+							<span class="input-group-text" style="font-size: 18px;">First Name</span>
+						</div>
+						<div class="col-md-3">
+							<input type="text" name="lbs_weight" id="" class="form-control">
+				    	</div>
+				    </div>
+				    <div class="row">
+				    	<div class="col-md-2 offset-md-3">
+							<span class="input-group-text" style="font-size: 18px;">Last Name</span>
+						</div>
+						<div class="col-md-3">
+							<input type="text" name="lbs_weight" id="" class="form-control">
+				    	</div>
+				    </div>
+				    <div class="row">
+				    	<div class="col-md-2 offset-md-3">
+							<span class="input-group-text" style="font-size: 18px;">Email</span>
+						</div>
+						<div class="col-md-3">
+							<input type="text" name="lbs_weight" id="" class="form-control">
+				    	</div>
+				    </div>
+				    <div class="row">
+				    	<div class="col-md-2 offset-md-3">
+							<span class="input-group-text" style="font-size: 18px;">Password</span>
+						</div>
+						<div class="col-md-3">
+							<input type="text" name="lbs_weight" id="" class="form-control">
+				    	</div>
+				    </div>
+				    <div class="row" style="text-align: center;margin:30px 0;">
+				    	<div class="col-md-9 offset-md-1">
+				    		<button type="button" style="background-color: #4f99c6!important;margin-right: 12px;" class="btn btn-primary legitRipple"><i class="icon-checkmark mr-2"></i>Update</button>
+				    		<button type="button" style="background-color: #8b9aa3!important;margin-left: 12px;" class="btn btn-primary legitRipple"><i class="icon-reset mr-2" onclick="resetForm()"></i>Reset</button>
+				    	</div>
+				    </div>
+			    </form>
 			</div>
 		</div>
 	</div>
@@ -106,13 +108,15 @@
 
 	$('.form-check-input-switch').bootstrapSwitch();
 	$('.custom-select').select2({
-		placeholder: 'Choose a company..',
-		allowClear: true,
 	    minimumResultsForSearch: Infinity
 	});
 	$('.custom-select1').select2({
 	    minimumResultsForSearch: Infinity
 	});
+	function resetForm() {
+		alert();
+    	//document.getElementById("edit_location_form").reset();
+	}
 
 </script>
 
