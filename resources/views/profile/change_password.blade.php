dd156c401e74f324473a3bc85fd04e3bff9b2bbf
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-05-27 16:55:41 +0500

    commit

--- resources/views/profile/change_password.blade.php
+++ resources/views/profile/change_password.blade.php
@@ -27,7 +27,7 @@
                     <hr allign="left">
                 </div>
             </div>
-            <form method="POST" action="{{url('/profile/change_password/'.$user->id)}}" id="">
+            <form method="POST" action="{{url('/profile/change_password/'.$user->id)}}" id="reset_password">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="row">
                     <div class="col-md-2 offset-md-3">
@@ -36,7 +36,7 @@
                         </span>
                     </div>
                     <div class="col-md-3">
-                        <input type="text" name="change_password" id="" value="" class="form-control">
+                        <input type="password" name="password" id="password" value="" class="form-control" onchange='check_pass();'>
                     </div>
                 </div>
                 <div class="row">
@@ -46,12 +46,12 @@
                         </span>
                     </div>
                     <div class="col-md-3">
-                        <input type="text" name="change_password" id="" value="" class="form-control">
+                        <input type="password" name="change_password" id="change_password" value="" class="form-control" onchange='check_pass();'>
                     </div>
                 </div>
                 <div class="row" style="text-align: center;margin:30px 0;">
                     <div class="col-md-9 offset-md-1">
-                        <button type="submit" style="background-color: #4f99c6!important;margin-right: 12px;" class="btn btn-primary legitRipple"><i class="icon-checkmark mr-2"></i>Submit</button>
+                        <button type="submit" name="submit" id="submit" disabled style="background-color: #4f99c6!important;margin-right: 12px;" class="btn btn-primary legitRipple"><i class="icon-checkmark mr-2"></i>Submit</button>
                         <button type="button" style="background-color: #8b9aa3!important;margin-left: 12px;" class="btn btn-primary legitRipple" onclick="resetForm();"><i class="icon-reset mr-2"></i>Reset</button>
                     </div>
                 </div>
@@ -63,12 +63,17 @@
 <script>
     function check_pass() {
         if (document.getElementById('password').value ==
-            document.getElementById('confirm_password').value) {
+            document.getElementById('change_password').value) {
             document.getElementById('submit').disabled = false;
         } else {
             document.getElementById('submit').disabled = true;
         }
     }
+
+    function resetForm() {
+		//alert();
+    	document.getElementById("reset_password").reset();
+	}
 </script>
 
 @endsection
