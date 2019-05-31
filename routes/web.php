8078d64ca09415576e0dd8433ff35877b9b01329
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-05-31 17:03:09 +0500

    commit

--- routes/web.php
+++ routes/web.php
@@ -51,9 +51,7 @@ Route::get('/profile/change_password/{id}','Profile@edit');
 Route::post('/profile/change_password/{id}','Profile@update');
 
 
-//Route::post('/reports/add','Report_images@store')->name('add_image');
-
-//Route::post('/reports/view','')
+Route::post('/reports/view','Report_images@store')->name('add_image');
 
 
 
