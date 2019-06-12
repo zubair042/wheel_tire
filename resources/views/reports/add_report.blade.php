7fdb58c77ab60629dda9863dc16ff882fbb0e753
zubair042 <042zubair@github.com>
2019-06-12 12:59:33 +0500

    commit

--- resources/views/reports/add_report.blade.php
+++ resources/views/reports/add_report.blade.php
@@ -32,7 +32,7 @@
 					<div class="form-check form-check-inline form-check-right">
 						<label class="form-check-label">
 							<span class="font-weight-semibold">POWER UNIT</span>
-							<div class=""><span class=""><input type="radio" id="power_unit_radio" class="form-check-input-styled-danger" name="vehicle_type" value="power_unit" data-fouc=""></span></div>
+							<div class=""><span class=""><input type="radio" id="power_unit_radio" class="form-check-input-styled-danger" name="vehicle_type" value="power_unit"  data-fouc=""></span></div>
 						</label>
 					</div>
 				</div>
@@ -142,79 +142,78 @@
 					</div>
 				</div>
 				<div class="row" id="trailer_html" style="margin-top: 10px;padding: 12px;">
-					<div class="row">
-						<div class="col-md-12" style="background-color: #fafafa;">
-							<div class="col-md-8 offset-md-2">
-								<div class="text-center">
-									<h5>SELECT WHEEL YOU WORKED ON</h5>
+					<div class="col-md-12" style="background-color: #fafafa;">
+						<div class="col-md-8 offset-md-2">
+							<div class="text-center">
+								<h5>SELECT WHEEL YOU WORKED ON</h5>
+							</div>
+							<li class="media">
+								<div class="mr-3 align-self-center img-width text-right">
+									<div class="form-check form-check-inline form-check-right">
+										<label class="form-check-label">
+											<span class="font-size-lg">Left Front Wheel</span>
+											<input type="checkbox" class="form-check-input-styled-danger" name="trailer_left_front" value="1" data-fouc="">
+										</label>
+									</div>
 								</div>
-								<li class="media">
-									<div class="mr-3 align-self-center img-width text-right">
-										<div class="form-check form-check-inline form-check-right">
-											<label class="form-check-label">
-												<span class="font-size-lg">Left Front Wheel</span>
-												<input type="checkbox" class="form-check-input-styled-danger" name="trailer_left_front" value="1" data-fouc="">
-											</label>
-										</div>
-									</div>
 
-									<div class="media-body text-center">
-									 	<img src="{{asset('global_assets/images/placeholders/fr.png')}}" class="" width="100%" height="100%" alt="">
+								<div class="media-body text-center">
+								 	<img src="{{asset('global_assets/images/placeholders/fr.png')}}" class="" width="100%" height="100%" alt="">
 
-									 	<a>
-									 		<input type="image" name="trailer_left_front" class="chooseImage" src="{{asset('global_assets/images/image.png')}}">
-									 		<input type="file" name="trailer_left_front[]" id="trailer_left_front" class="d-none" multiple="">
-									 	</a>
-									 	<a href="javascript:;">
-									 		<input type="image" name="trailer_right_front" class="chooseImage float-right" src="{{asset('global_assets/images/image.png')}}">
-									 		<input type="file" name="trailer_right_front[]" id="trailer_right_front" class="d-none" multiple="">
-									 	</a>
-									</div>
-
-									<div class="align-self-center ml-3 img-width">
-										<div class="form-check form-check-inline form-check-right">
-											<label class="form-check-label">
-												<input type="checkbox" class="form-check-input-styled-danger" name="trailer_right_front" value="1" data-fouc="">&nbsp;&nbsp; <span class="font-size-lg">Right Front Wheel</span>
-											</label>
-										</div>
-									</div>
-								</li>
-								<li class="media form-group">
-									<div class="mr-3 align-self-center img-width text-right">
-										<div class="form-check form-check-inline form-check-right">
-											<label class="form-check-label">
-												<span class="font-size-lg">Left Rear Wheel</span>
-												<input type="checkbox" class="form-check-input-styled-danger" name="trailer_left_rear" value="1" data-fouc="">
-											</label>
-										</div>
-									</div>
-
-									<div class="media-body">
-										<img src="{{asset('global_assets/images/placeholders/fr.png')}}" class="" width="100%" height="100%" alt="">
-										<a href="javascript:;">
-											<input type="image" src="{{asset('global_assets/images/image.png')}}" name="trailer_left_rear" class="chooseImage">
-											<input type="file" name="trailer_left_rear[]" id="trailer_left_rear" class="d-none" multiple="">
-										</a>
-										<a href="javascript:;">
-											<input type="image" src="{{asset('global_assets/images/image.png')}}" name="trailer_right_rear" class="chooseImage float-right">
-											<input type="file" name="trailer_right_rear[]" id="trailer_right_rear" class="d-none" multiple="">
-										</a>
-									</div>
-
-									<div class="align-self-center ml-3 img-width">
-										<div class="form-check form-check-inline form-check-right">
-											<label class="form-check-label">
-												<input type="checkbox" class="form-check-input-styled-danger" name="trailer_right_rear" value="1" data-fouc="">&nbsp;&nbsp; <span class="font-size-lg">Right Rear Wheel</span>
-											</label>
-										</div>
-									</div>
-								</li>
-								<div class="text-center">
-									<h5>If you pull the hub you MUST take photo of completed wheel-end!</h5>
+								 	<a>
+								 		<input type="image" name="trailer_left_front" class="chooseImage" src="{{asset('global_assets/images/image.png')}}">
+								 		<input type="file" name="trailer_left_front[]" id="trailer_left_front" class="d-none" multiple="">
+								 	</a>
+								 	<a href="javascript:;">
+								 		<input type="image" name="trailer_right_front" class="chooseImage float-right" src="{{asset('global_assets/images/image.png')}}">
+								 		<input type="file" name="trailer_right_front[]" id="trailer_right_front" class="d-none" multiple="">
+								 	</a>
 								</div>
+
+								<div class="align-self-center ml-3 img-width">
+									<div class="form-check form-check-inline form-check-right">
+										<label class="form-check-label">
+											<input type="checkbox" class="form-check-input-styled-danger" name="trailer_right_front" value="1" data-fouc="">&nbsp;&nbsp; <span class="font-size-lg">Right Front Wheel</span>
+										</label>
+									</div>
+								</div>
+							</li>
+							<li class="media form-group">
+								<div class="mr-3 align-self-center img-width text-right">
+									<div class="form-check form-check-inline form-check-right">
+										<label class="form-check-label">
+											<span class="font-size-lg">Left Rear Wheel</span>
+											<input type="checkbox" class="form-check-input-styled-danger" name="trailer_left_rear" value="1" data-fouc="">
+										</label>
+									</div>
+								</div>
+
+								<div class="media-body">
+									<img src="{{asset('global_assets/images/placeholders/fr.png')}}" class="" width="100%" height="100%" alt="">
+									<a href="javascript:;">
+										<input type="image" src="{{asset('global_assets/images/image.png')}}" name="trailer_left_rear" class="chooseImage">
+										<input type="file" name="trailer_left_rear[]" id="trailer_left_rear" class="d-none" multiple="">
+									</a>
+									<a href="javascript:;">
+										<input type="image" src="{{asset('global_assets/images/image.png')}}" name="trailer_right_rear" class="chooseImage float-right">
+										<input type="file" name="trailer_right_rear[]" id="trailer_right_rear" class="d-none" multiple="">
+									</a>
+								</div>
+
+								<div class="align-self-center ml-3 img-width">
+									<div class="form-check form-check-inline form-check-right">
+										<label class="form-check-label">
+											<input type="checkbox" class="form-check-input-styled-danger" name="trailer_right_rear" value="1" data-fouc="">&nbsp;&nbsp; <span class="font-size-lg">Right Rear Wheel</span>
+										</label>
+									</div>
+								</div>
+							</li>
+							<div class="text-center">
+								<h5>If you pull the hub you MUST take photo of completed wheel-end!</h5>
 							</div>
 						</div>
 					</div>
+					
 				</div>
 			    <div class="row text-center">
 			    	<div class="col-md-12">
@@ -299,16 +298,21 @@ $("#submit1232222").on("click", function(){
 	$('.form-check-input-styled-danger').uniform({
         wrapperClass: 'border-danger-600 text-danger-800'
     });
+
     $('#trailer_radio').click(function(){
-        $("#trailer_html").css("display","block");
-        $("#power_unit_html").css("display","none");
-        
+    	if ($('#power_unit_html').css("display","block")) {
+	        $("#trailer_html").css("display","block");
+	        $("#power_unit_html").css("display","none");
+	    }
     });
+
     $('#power_unit_radio').click(function(){
-        $("#trailer_html").css("display","none");
-        $("#power_unit_html").css("display","block");
-        
+    	if ($('#trailer_html').css("display","block")) {
+	        $("#trailer_html").css("display","none");
+	        $("#power_unit_html").css("display","block");
+	    }
     });
+
     $('.select_select2_select').select2({
 	    minimumResultsForSearch: Infinity
 	});
